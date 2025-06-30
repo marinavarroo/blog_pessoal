349b37dc09f317675916595f25546fe1cb9f6389
Maria Navarro <mariacdnavarro@gmail.com>
2025-06-30 10:13:25 -0300

    Criação da entidade Postagem, métodos de listagem no Controller/Service e inserção de dados no banco

---
+++ src/postagem/entities/postagem.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { PostagemController } from "../controller/postagem.controller";
+import { Postagem } from "./postagem.entity";
+import { PostagemService } from "../sevices/postagem.service";
+
+@Module ({
+    imports: [TypeOrmModule.forFeature([Postagem])],
+    providers: [PostagemService],
+    controllers: [PostagemController],
+    exports: [],
+})
+export class PostagemModule {}
