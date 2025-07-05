b3b08ae57f6967f50c268d8c366ac0d4fe7d3d69
Maria Navarro <mariacdnavarro@gmail.com>
2025-07-05 15:47:39 -0300

    Adiciona método findById na service e controller

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
 import { Postagem } from '../entities/postagem.entity';
 import { PostagemService } from '../services/postagem.service';
 
@@ -10,6 +10,12 @@ export class PostagemController {
 @HttpCode(HttpStatus.OK)
 findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
+}
+
+@Get('/:id')
+@HttpCode(HttpStatus.OK)
+findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findById(id);
 }
 
 }
