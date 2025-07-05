a0c7e67595019555ea40304fdc30e30f4475a340
Maria Navarro <mariacdnavarro@gmail.com>
2025-07-05 17:27:06 -0300

    Adiciona métodos Create, Update e Delete na service e controller

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { Postagem } from '../entities/postagem.entity';
 import { PostagemService } from '../services/postagem.service';
 
@@ -22,6 +22,24 @@ findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
 @HttpCode(HttpStatus.OK)
 findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]>{
     return this.postagemService.findAllByTitulo(titulo);
+}
+
+@Post()
+@HttpCode(HttpStatus.CREATED)
+create(@Body() postagem: Postagem): Promise<Postagem>{
+    return this.postagemService.create(postagem);
+}
+
+@Put()
+@HttpCode(HttpStatus.OK)
+update(@Body() postagem: Postagem): Promise<Postagem>{
+    return this.postagemService.update(postagem);
+}
+
+@Delete('/:id')
+@HttpCode(HttpStatus.NO_CONTENT)
+delete(@Param('id', ParseIntPipe) id: number){
+    return this.postagemService.delete(id);
 }
 
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { ILike, Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
 
 @Injectable()
@@ -29,6 +29,7 @@ export class PostagemService {
     }
 
     async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+
         const nome = await this.postagemRepository.find({
             where: {
                 titulo: ILike(`%${titulo}%`)
@@ -41,5 +42,23 @@ export class PostagemService {
 
         return nome;
     }
+
+    async create(postagem: Postagem): Promise<Postagem> {
+        return await this.postagemRepository.save(postagem)
+    }
+
+    async update(postagem: Postagem): Promise<Postagem>{
+        
+        await this.findById(postagem.id)
+
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number): Promise<DeleteResult>{
+        
+        await this.findById(id)
+
+        return await this.postagemRepository.delete(id)
+    } 
 
 }
